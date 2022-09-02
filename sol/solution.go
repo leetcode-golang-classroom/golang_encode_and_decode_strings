dc929c1e3f311eb3515e86258ed4b6395c86590c
gson Liang <yuanyu90221@gmail.com>
2022-09-02 23:59:57 +0800

    add need method

--- sol/solution.go
+++ sol/solution.go
@@ -1,6 +1,9 @@
 package sol
 
-import "strings"
+import (
+	"fmt"
+	"strconv"
+)
 
 type ListHandle struct {
 	sep string
@@ -12,9 +15,29 @@ func Constructor(sep string) ListHandle {
 	}
 }
 func (h *ListHandle) encode(list []string) string {
-	return strings.Join(list, h.sep)
+	encodeString := ""
+	for _, val := range list {
+		encodeString += fmt.Sprintf("%d%s%s", len(val), h.sep, val)
+	}
+	return encodeString
 }
 
 func (h *ListHandle) decode(str string) []string {
-	return strings.Split(str, h.sep)
+	decodeResult := []string{}
+	pos := 0
+	strLen := len(str)
+	tempLen := ""
+	for pos < strLen {
+		if string(str[pos]) != h.sep {
+			tempLen += string(str[pos])
+		} else { // first meet #
+			num, _ := strconv.Atoi(tempLen)
+			decodeResult = append(decodeResult, str[pos+1:pos+num+1])
+			tempLen = ""
+			pos += num + 1
+			continue
+		}
+		pos++
+	}
+	return decodeResult
 }
